e764a1a9ad53334378e06811c99e02db15fbc640
kingfisherking <Quaaysan@gmail.com>
2023-09-30 11:58:48 -0500

    update to filepath parsing, name error fix pending

--- ASC.py
+++ ASC.py
@@ -4,12 +4,13 @@ import os
 import shutil
 #accessing user's desktop, this currently assumes the program is deployed in a folder on the desktop
 CWD = os.getcwd()
-desktopPath = CWD[0:CWD.find("Desktop")+8] #+8 for the length of 'Desktop/' to include in the path
+desktopPath = CWD[0:CWD.find("Desktop")+7]+"\\" #+7 for the length of 'Desktop/' to include in the path
 
 # searching desktop for folder labeled "Screenshots", creating one if it doesn't exist
 #this variable essentially names the folder, change the word "Screenshots" to what you want
-screenshotPath = desktopPath+'/Screenshots' 
-if not os.path.exists(screenshotPath):
+screenshotPath = desktopPath+'Screenshots' 
+print(os.path.exists(screenshotPath))
+if os.path.exists(screenshotPath) != True:
     os.makedirs(screenshotPath) #creates a folder on desktop named after that variable
 
 #creating a list of strings containing the filenames for all files in desktop
@@ -20,3 +21,6 @@ for file in fileList:
         if file.endswith(".png") or file.endswith(".jpg"):
             print(desktopPath+file)
             shutil.move(desktopPath+file, screenshotPath) #moves files from desktop to screenshot folder
+
+#devlog 9/30: right now, this program fails if there's a screenshot with the same name in that folder already
+#not tpically a problem, but testing is difficult and surely there's a case
